62a4c62a38a378df08437c305026c569e36b3b55
Emanuel Espinosa <emanuel.espinosa.roberto@gmail.com>
2026-05-14 23:12:07 -0300

    Agregar excepciones personalizadas para el manejo de errores
    - Creación del paquete exception.
    
    - Implementación de ProductoNoEncontradoException para búsquedas fallidas en el CRUD.
    
    - Implementación de StockInsuficienteException para validar operaciones de inventario o ejecución de pedidos.
    
    Implementar ProductoService con operaciones CRUD completas
    
    - Implementación de guardarProducto con asignación de ID autoincremental y validación integral.
    
    - Método getListaProductos para recuperación de la lista completa.
    
    - Búsqueda por ID con lanzamiento de ProductoNoEncontradoException personalizada.
    
    - Lógica de actualización y eliminación de productos integrando validaciones de negocio.

---
+++ src/com/techlab/ecommerce/service/ProductoService.java
@@ -0,0 +1,51 @@
+package com.techlab.ecommerce.service;
+import java.util.ArrayList;
+import java.util.List;
+import com.techlab.ecommerce.exception.ProductoNoEncontradoException;
+import com.techlab.ecommerce.model.Producto;
+
+public class ProductoService {
+    private List<Producto> productos = new ArrayList<>();
+    private static int counterId = 1;
+
+    public Producto guardarProducto(Producto p){
+        p.setId(counterId);
+        productos.add(p);
+        counterId++;
+        return p;
+    }
+
+    //Listar productos
+    public List<Producto> getListaProductos(){
+        return productos;
+    }
+
+    //Buscar producto por Id
+    public Producto getProductoPorId(int id){
+        for(Producto p : this.productos){
+            if(p.getId() == id){
+                return p;
+            }
+        }
+        throw new ProductoNoEncontradoException(String.format("No se encontro el producto con Id %d", id));
+    }
+
+    //Actualizar los datos de un producto existente
+    public Producto actualizarProducto(int id, Producto nuevoProducto){
+        Producto p = getProductoPorId(id);
+
+        p.setNombre(nuevoProducto.getNombre());
+        p.setPrecio(nuevoProducto.getPrecio());
+        p.setStock(nuevoProducto.getStock());
+        p.setCategoria(nuevoProducto.getCategoria());
+        p.setMarca(nuevoProducto.getMarca());
+
+        return p;
+    }
+
+    //Eliminar producto existente por Id
+    public void eliminarProducto(int id){
+        Producto p = getProductoPorId(id);
+        productos.remove(p);
+    }
+}
