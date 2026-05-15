cd4800f09556ecef4f0ce21fa1ec475c37f4f6ce
Emanuel Espinosa <emanuel.espinosa.roberto@gmail.com>
2026-05-15 12:18:20 -0300

    Se solucionaron errores al calcular el total a pagar de cada pedido realizado

--- src/com/techlab/ecommerce/model/Pedido.java
+++ src/com/techlab/ecommerce/model/Pedido.java
@@ -6,13 +6,13 @@ import com.techlab.ecommerce.util.ItemPedido;
 public class Pedido {
     private int id;
     private List<ItemPedido> items = new ArrayList<>();
-    private double totalAPagar = 0;
 
     public int getId() { return id;}
     public void setId(int id) {this.id = id;}
     public List<ItemPedido> getItems() {return items;}
 
     public double getTotalAPagar(){
+        double totalAPagar = 0;
         for(ItemPedido item : items){
             totalAPagar+=(item.getProducto().getPrecio())*item.getCantidad();
         }
